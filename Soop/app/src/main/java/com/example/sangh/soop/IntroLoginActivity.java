d64065a0d4eabf7db441ef655dabe15054f36e4d
sanghyub <sanghyub91@gmail.com>
2017-02-09 14:50:31 +0900

    IntroActivity, navigation drawer init

---
+++ Soop/app/src/main/java/com/example/sangh/soop/IntroLoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.sangh.soop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroLoginActivity extends AppCompatActivity {
+Button startBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_login);
+
+        startBtn= (Button)(findViewById(R.id.btn_start));
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroLoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
